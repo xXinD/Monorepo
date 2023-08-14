57fd538dbec2eb0a16a05c18cf26db053e193616
XinD <xindcool@gmail.com>
2023-08-15 03:50:52 +0800

    :sparkles: New features: Add watermark, produce playlists Switch to flunt-ffmpeg to recreate the logic of the previous spawn manipulation of ffmpeg Added push stream M3U8

--- Backend/src/routes/generalRoutes.ts
+++ Backend/src/routes/generalRoutes.ts
@@ -3,7 +3,6 @@ import {
   getServerConfig,
   getTheListOfSystemFonts,
   postPlaylist,
-  postWatermarkToVideo,
   updateServerConfig,
 } from "../controllers/general";
 
@@ -12,7 +11,6 @@ const generalRoutes = new Router();
 generalRoutes.get("/", getServerConfig);
 generalRoutes.post("/", updateServerConfig);
 generalRoutes.get("/fontList", getTheListOfSystemFonts);
-generalRoutes.post("/postWatermarkToVideo", postWatermarkToVideo);
 generalRoutes.post("/postPlaylist", postPlaylist);
 
 export default generalRoutes;
